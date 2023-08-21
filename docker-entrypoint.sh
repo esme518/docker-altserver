d5a00da70cff7be4aa5483f24dd24e3abd72bab6
esme518 <esme518@gmail.com>
2023-08-21 13:41:11 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,6 @@ if [ ! -f main.py ]; then
 	curl -sSL https://raw.githubusercontent.com/powenn/AltServer-Linux-PyScript/rewrite/main.py > main.py
 fi
 
+sed -i 's/^[^#].*password.*getpass.getpass/#&/g' main.py
+
 exec "$@"
